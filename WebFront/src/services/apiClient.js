852465452c1ffbe35d922cbd33bbf132bee33cfc
keepreverse <supreme2309@mail.ru>
2025-06-04 22:46:43 +0300

    fixes

--- WebFront/src/services/apiClient.js
+++ WebFront/src/services/apiClient.js
@@ -1,6 +1,6 @@
 import { toast } from "react-toastify";
 
-// const API_URL = "http://127.0 .0.1:5000/api";
+// const API_URL = "http://127.0. 0.1:5000/api";
 
 const API_URL = "https://webnews-1fwz.onrender.com/api";
 
